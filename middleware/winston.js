d26611ae148a0af62f53ac30db82ebbf892481b5
Elia El Atram <eliaatram@outlook.com>
2024-11-09 14:43:25 +0100

    wip

--- middleware/winston.js
+++ middleware/winston.js
@@ -38,7 +38,7 @@ logger.stream = {
   write: function (message, encoding) {
     // use the 'info' log level so the output will be picked up by both
     // transports (file and console)
-    logger.info(message);
+    logger.debug(message);
   },
 };
 
--- server.js
+++ server.js
@@ -1,51 +1,10 @@
 require("dotenv").config();
-const express = require("express");
-const app = express();
-const PORT = 8080;
-const mongoose = require("mongoose");
+const startApp = require("./boot/setup").startApp;
 
-// Custom middleware
-const cors = require("cors");
-const helmet = require("helmet");
-const session = require("express-session");
-const verifyToken = require("./middleware/authentication");
-
-// Routers
-const todoRouter = require("./routes/todo.routes");
-const messageRouter = require("./routes/message.routes");
-const userRouter = require("./routes/user.routes");
-
-try {
-  mongoose.connect("mongodb://localhost:27017/epita");
-} catch (error) {
-  console.log("Error connecting to MongoDB", error);
-}
-
-app.use(
-  session({
-    secret: "1234",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: false,
-      httpOnly: true,
-    },
-  })
-);
-
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-
-app.use("/todos", todoRouter);
-app.use("/messages", verifyToken, messageRouter);
-app.use("/users", userRouter);
-
-app.get("/", (req, res) => {
-  console.log("Here");
-  res.status(200).json({ message: "Bye" });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on PORT: ${PORT}`);
-});
+(() => {
+  try {
+    startApp();
+  } catch (error) {
+    console.log("Error in server.js => startApp");
+  }
+})();
